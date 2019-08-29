e84bc54b68468a0fd33b0527d6d14b62eac1fa6f
Justin Kilpatrick <justin@althea.net>
2019-08-29 14:51:04 -0400

    Finally works properly

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
-use deep_space::msg::SendMsg;
-use deep_space::msg::Msg;
-use deep_space::stdfee::StdFee;
-use deep_space::coin::Coin;
 use deep_space::address::Address;
+use deep_space::coin::Coin;
+use deep_space::msg::Msg;
+use deep_space::msg::SendMsg;
 use deep_space::private_key::PrivateKey;
+use deep_space::stdfee::StdFee;
 use deep_space::stdsignmsg::StdSignMsg;
 use docopt::Docopt;
-use serde::Deserialize;
 use reqwest::Client;
+use serde::Deserialize;
 use std::fs::File;
 use std::io::Write;
+#[macro_use]
+extern crate serde_derive;
 
 #[derive(Debug, Deserialize)]
 struct Args {
     flag_key: String,
     flag_fullnode: String,
 }
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+struct CosmosAccountInfoWrapper {
+    #[serde(rename = "type")]
+    account_type: String,
+    value: CosmosAccountInfo
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+struct CosmosAccountInfo {
+    account_number: String,
+    coins: Vec<Coin>, 
+    sequence: String,
+}
+
 
 fn main() {
     let usage = format!(
@@ -28,6 +45,7 @@ About:
     Version {}",
         env!("CARGO_PKG_VERSION"),
     );
+    let client = Client::new();
     let args: Args = Docopt::new(usage)
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
@@ -41,21 +59,34 @@ About:
     // Print some diagnostics
     println!("Address: {}", address.to_bech32("cosmos").unwrap());
     println!("Public key: {}", public_key.to_bech32("cosmospub").unwrap());
-    // Sign some stuff
+
+    let res:CosmosAccountInfoWrapper = client
+        .get(&format!(
+            "{}/auth/accounts/{}",
+            fullnode,
+            address.to_bech32("cosmos").unwrap()
+        ))
+        .send()
+        .unwrap()
+        .json().unwrap();
 
     let std_sign_msg = StdSignMsg {
         chain_id: "altheatest4".to_string(),
-        account_number: 1u64,
-        sequence: 0u64,
+        account_number: res.value.account_number.parse().unwrap(),
+        sequence: res.value.sequence.parse().unwrap(),
         fee: StdFee {
-            amount: None,
+            amount: vec![Coin {
+                denom: "ualtg".to_string(),
+                amount: 42u32.into(),
+            }],
             gas: 200_000u64.into(),
         },
         msgs: vec![Msg::SendMsg(SendMsg {
             from_address: address,
             to_address: Address::from_bech32(
                 "cosmos1x47p0tjylnsmxyy33q4mhqtxhzwk5009fh35sq".to_string(),
-            ).unwrap(),
+            )
+            .unwrap(),
             amount: vec![Coin {
                 denom: "ualtg".to_string(),
                 amount: 500u32.into(),
@@ -69,8 +100,12 @@ About:
     let mut file = File::create("signed_msg.json").unwrap();
     file.write_all(s.as_bytes()).unwrap();
     println!("{:?}", tx);
-    let client = Client::new();
-    let res = client.post(&format!("{}/txs", fullnode)).json(&tx).send().unwrap().text();
+    let res = client
+        .post(&format!("{}/txs", fullnode))
+        .json(&tx)
+        .send()
+        .unwrap()
+        .text();
 
     println!("{:?}", res);
 }
